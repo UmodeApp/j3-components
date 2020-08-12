5304b83e5a46ff9128f4fe2120918e5e920347e0
Saulo Arruda <saulo@jumpervb.com.br>
2020-08-12 11:21:48 -0300

    try to fix A copy of J3Components::ActsAsRedirectableCreate has been removed from the module tree but is still active! error and bundle update

--- lib/j3_components/acts_as_redirectable_create.rb
+++ lib/j3_components/acts_as_redirectable_create.rb
@@ -1,11 +1,8 @@
 module ::J3Components
   module ActsAsRedirectableCreate
-    extend ActiveSupport::Concern
-    class_methods do
-      # Enable save session and redirect behavior in controller
-      def acts_as_redirectable_create(_options = {})
-        include J3Components::SaveSessionAndRedirect
-      end
+    # Enable save session and redirect behavior in controller
+    def acts_as_redirectable_create(_options = {})
+      include J3Components::SaveSessionAndRedirect
     end
   end
 end
